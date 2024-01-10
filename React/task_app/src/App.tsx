1b92429a926406f4a8c35f7bfaa96c14100a54bd
heet0309 <120097865+heet0309@users.noreply.github.com>
2024-01-10 18:54:13 +0530

    NEW_TASK_APP

--- React/task_app/src/App.tsx
+++ React/task_app/src/App.tsx
@@ -1,20 +1,17 @@
 import React from 'react'
 import List from './todo'
 import SearchFilter from './Filter'
-import GuessNumber from './NumberGame'
-import Quiz from './Quiz'
+
 import Timer from './Timer_component'
 import ToggleButton from './toggleButton'
 
 export function App() {
   return (
     <div>
-        {/* <List/>
+        <List/>
         <ToggleButton/>
         <Timer/>
         <SearchFilter/>
-        <GuessNumber/> */}
-        <Quiz/>
     </div>
 
   )
---
+++ new_task_app/src/App.tsx
@@ -0,0 +1,13 @@
+import GuessNumber from './NumberGame';
+import Quiz from './Quiz';
+
+function App() {
+  return (
+   <>
+   <GuessNumber/>
+   <Quiz/>
+   </>
+  );
+}
+
+export default App;
